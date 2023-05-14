b0d8b307988870f1b8a9e561b5522a9c029f72ee
Matt Howey <howey.matt@gmail.com>
2023-05-14 11:55:58 -0400

    Adds process.env port definition

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fastify = require('fastify')({
   logger: true
 })
+const port = process.env.PORT || 3000;
 fastify.register(require('./api/airlineRoutes.js'));
 fastify.register(require('./api/peopleRoutes.js'));
 
-fastify.listen({port: 3000, error: (error) => fastify.log.error(error)});
+fastify.listen({port: port, error: (error) => fastify.log.error(error)});
